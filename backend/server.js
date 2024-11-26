743c5c696a9dca2bf8e09b9d70c1047496a99591
Christian Tannahill <cstannahill10@gmail.com>
2024-11-25 20:46:09 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,21 +4,21 @@ const roomRoutes = require("./routes/room");
 
 const app = express();
 
-// CORS Configuration
+
 const corsOptions = {
-  origin: "http://localhost:3000", // Allow requests from this origin
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
-  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
-  credentials: true, // Allow credentials (if you are using cookies or auth tokens)
+  origin: "http://localhost:3000", 
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"], 
+  credentials: true,
 };
 
-// Use the CORS middleware
+
 app.use(cors(corsOptions));
 
-// Handle preflight OPTIONS requests for all routes
+
 app.options("*", cors(corsOptions));
 
-// Log incoming requests for debugging
+
 app.use((req, res, next) => {
   console.log(
     `Request Method: ${req.method}, Request Headers: ${JSON.stringify(
@@ -28,12 +28,12 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.json()); // For parsing JSON request bodies
+app.use(express.json()); 
 
-// Room Routes
+
 app.use("/api/rooms", roomRoutes);
 
-// Start the server
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
